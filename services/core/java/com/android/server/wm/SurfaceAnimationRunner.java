307cf24b9b2c2859c36f7d6d81a82cb7b71789de
minaripenguin37 <alexfinhart@gmail.com>
2022-11-10 19:40:52 +0800

    Services: WM: Boost critical UI animations
    Signed-off-by: minaripenguin37 <alexfinhart@gmail.com>

--- services/core/java/com/android/server/wm/SurfaceAnimationRunner.java
+++ services/core/java/com/android/server/wm/SurfaceAnimationRunner.java
@@ -340,7 +340,7 @@ class SurfaceAnimationRunner {
             }
             startPendingAnimationsLocked();
         }
-        mPowerManagerInternal.setPowerBoost(Boost.INTERACTION, 0);
+        mPowerManagerInternal.setPowerBoost(Boost.INTERACTION, 2000);
     }
 
     private void scheduleApplyTransaction() {
