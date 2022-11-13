5a46a352447a2bb67a9281bc016c6ed88fd7b02c
cjh1249131356 <cjh1249131356@gmail.com>
2022-11-13 08:11:11 +0800

    base: Always turn on sensors when disabling sleep mode
    * Also verify once to make sure sensors are turned on.
    
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>
    Signed-off-by: minaripenguin37 <alexfinhart@gmail.com>

--- core/java/com/android/internal/util/rice/RiceUtils.java
+++ core/java/com/android/internal/util/rice/RiceUtils.java
@@ -182,7 +182,6 @@ public class RiceUtils {
         private static boolean mLocationState;
         private static boolean mCellularState;
         private static boolean mBluetoothState;
-        private static boolean mSensorState;
         private static int mRingerState;
         private static int mZenState;
 
@@ -396,7 +395,6 @@ public class RiceUtils {
             final boolean disableSensors = Settings.Secure.getIntForUser(mContext.getContentResolver(),
                     Settings.Secure.SLEEP_MODE_SENSORS_TOGGLE, 1, UserHandle.USER_CURRENT) == 1;
             if (disableSensors) {
-                mSensorState = isSensorEnabled();
                 setSensorEnabled(false);
             }
 
@@ -456,8 +454,14 @@ public class RiceUtils {
             // Enable Sensors
             final boolean disableSensors = Settings.Secure.getIntForUser(mContext.getContentResolver(),
                     Settings.Secure.SLEEP_MODE_SENSORS_TOGGLE, 1, UserHandle.USER_CURRENT) == 1;
-            if (disableSensors && mSensorState != isSensorEnabled()) {
-                setSensorEnabled(mSensorState);
+            if (disableSensors) {
+                setSensorEnabled(true);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {}
+                if (!isSensorEnabled()) {
+                    setSensorEnabled(true);
+                }
             }
 
             // Set Ringer mode (0: Off, 1: Vibrate, 2:DND: 3:Silent)
