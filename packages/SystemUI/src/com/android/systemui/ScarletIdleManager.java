19509cefae59b78dec085dd70607c92fd0c27bcb
minaripenguin <minaripenguin@users.noreply.github.com>
2022-11-18 09:44:51 +0800

    Scarlet Services: Remove synchandler implementations
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/ScarletIdleManager.java
+++ packages/SystemUI/src/com/android/systemui/ScarletIdleManager.java
@@ -65,7 +65,6 @@ public class ScarletIdleManager {
 
         mStartManagerInstance = new Runnable() {
             public void run() {
-                    syncHandler(false);
                     powerSaverHandler(true);
                     killBackgroundProcesses();
             }
@@ -97,10 +96,6 @@ public class ScarletIdleManager {
         }
     }
 
-    public static void syncHandler(boolean enable) {
-        ContentResolver.setMasterSyncAutomatically(enable);
-    }
-
     public static void powerSaverHandler(boolean enable) {
         PowerManagerInternal mLocalPowerManager = LocalServices.getService(PowerManagerInternal.class);
         if (mLocalPowerManager != null) {
@@ -115,7 +110,6 @@ public class ScarletIdleManager {
 
     public static void onScreenWake(Context mContext) {
         scarletHandler.removeCallbacks(mStopManagerInstance);
-        syncHandler(true);
         powerSaverHandler(false);
         PowerManagerInternal mLocalPowerManager = LocalServices.getService(PowerManagerInternal.class);
         if (mLocalPowerManager != null) {
